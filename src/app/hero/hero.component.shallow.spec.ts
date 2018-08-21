d275423fe66a2faf128ffa490da53c6ae4cf3ed6
sylvain.pointud <sylvain.pointud>
2018-08-21 14:50:07 +0200

    add 'should render the hero name in an achor tag' test

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { HeroComponent } from "./hero.component";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
+
+
+describe('HeroComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    fixture = TestBed.createComponent(HeroComponent);
+  })
+
+  it('should have the correct hero', () => {
+    // Arrange & Act
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+    // Assert
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDude')
+  })
+
+  it('should render the hero name in an achor tag', () => {
+    // Arrange & Act
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+    fixture.detectChanges();
+    // Assert
+    let debugElementAnchorTag = fixture.debugElement.query(By.css('a'))
+    expect(debugElementAnchorTag.nativeElement.textContent).toContain('SuperDude');
+    expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+
+  })
+})
